845da81dc69789a879ddc6dd764dee87920c65c8
jcgarciam <jcgarciam@gmail.com>
2014-06-15 23:02:55 -0300

    support for JAX-RX generator

---
+++ src/main/java/com/github/jcgarciam/tools/annotations/JaxRSEndpointExporter.java
@@ -0,0 +1,16 @@
+package com.github.jcgarciam.tools.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ **/
+@Target(ElementType.TYPE)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface JaxRsEndpointExporter {
+    public String packageName();
+    public String suffix() default "Endpoint";
+}
